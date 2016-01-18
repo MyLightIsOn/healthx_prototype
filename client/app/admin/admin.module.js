a425144c102d740130edbedb2262bfe8ffbe412e
Lawrence Moore <lawrence2978@hotmail.com>
2016-01-18 15:23:39 -0500

    Initial Commit

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('healthxApp.admin', [
+  'healthxApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('healthxApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
