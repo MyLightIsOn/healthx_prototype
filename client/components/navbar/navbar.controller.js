c1bf63bcf431dc036cb78a4f726778506c8f0976
Lawrence Moore <lawrence2978@hotmail.com>
2016-01-27 16:41:38 -0500

    Updated editorconfig

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,21 +1,23 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  }];
+	//start-non-standard
+	menu = [
+		{
+			'title': 'Home',
+			'state': 'main'
+		}
+	];
 
-  isCollapsed = true;
-  //end-non-standard
+	isCollapsed = true;
+	//end-non-standard
 
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
+	constructor(Auth) {
+		this.isLoggedIn = Auth.isLoggedIn;
+		this.isAdmin = Auth.isAdmin;
+		this.getCurrentUser = Auth.getCurrentUser;
+	}
 }
 
 angular.module('healthxApp')
-  .controller('NavbarController', NavbarController);
+	.controller('NavbarController', NavbarController);
