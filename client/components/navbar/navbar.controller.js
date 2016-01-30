2488a35e17cf730733a29632bd5571a398dec87f
Lawrence Moore <lawrence2978@hotmail.com>
2016-01-29 22:26:40 -0500

    Added sliding animation to header

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,24 +1,29 @@
 'use strict';
 
 class NavbarController {
-  //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  }, {
-    'title': 'Profile Info',
-    'state': 'main'
-  }];
+	//start-non-standard
+	menu = [{
+		'title': 'Home',
+		'state': 'main'
+	}, {
+		'title': 'Profile Info',
+		'state': 'main'
+	}];
 
-  isCollapsed = false;
-  //end-non-standard
+	isCollapsed = false;
+	//end-non-standard
 
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
+	constructor(Auth, $scope) {
+		this.isLoggedIn = Auth.isLoggedIn;
+		this.isAdmin = Auth.isAdmin;
+		this.getCurrentUser = Auth.getCurrentUser;
+
+		$scope.isActive = false;
+		$scope.activeButton = function() {
+			$scope.isActive = !$scope.isActive;
+		}
+	}
 }
 
 angular.module('healthxApp')
-  .controller('NavbarController', NavbarController);
+	.controller('NavbarController', NavbarController);
