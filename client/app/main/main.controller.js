b93374e9a3fcec5f8b1f435b34067afefddd4a95
Lawrence Moore <lawrence2978@hotmail.com>
2016-02-12 14:08:37 -0500

    Chart.js renders on homepage

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,6 +7,27 @@
     constructor($http, $scope, socket) {
       this.$http = $http;
       this.awesomeThings = [];
+      this.data = [
+        {
+          value: 300,
+          color:"#F7464A",
+          highlight: "#FF5A5E",
+          label: "Red"
+        },
+        {
+          value: 50,
+          color: "#46BFBD",
+          highlight: "#5AD3D1",
+          label: "Green"
+        },
+        {
+          value: 100,
+          color: "#FDB45C",
+          highlight: "#FFC870",
+          label: "Yellow"
+        }
+      ];
+
 
       /*Sets all of the views to false*/
       $scope.isActive = false;
@@ -44,8 +65,13 @@
         if(pageToOpen[0] != undefined){
           pageToOpen[0].className = 'active'
         }
-
       });
+
+      this.createChart();
+    }
+    createChart(){
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myNewChart = new Chart(ctx).Doughnut(this.data);
     }
 
     addThing() {
