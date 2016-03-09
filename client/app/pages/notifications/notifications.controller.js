98a89dcaf1299b633a8118518f13fa61c641f6a5
Lawrence Moore <lawrence2978@hotmail.com>
2016-03-08 22:06:26 -0500

    Notifications page wrapped up

--- client/app/pages/notifications/notifications.controller.js
+++ client/app/pages/notifications/notifications.controller.js
@@ -3,4 +3,9 @@
 angular.module('healthxApp')
   .controller('NotificationsCtrl', function ($scope) {
     $scope.message = 'Hello';
+
+      $scope.toggleSwitch = function($event){
+          $event.currentTarget.classList.toggle('on')
+      }
+
   });
