f200acf30b69d96e39048646d50cd6c131951f72
Lawrence Moore <lawrence2978@hotmail.com>
2016-03-20 13:25:55 -0400

    Added functionality to claims page for user test

--- client/app/pages/payclaim/payclaim.controller.js
+++ client/app/pages/payclaim/payclaim.controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
-angular.module('healthxApp')
-  .controller('PayClaimCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+(function() {
+
+    class PayClaimCtrl {
+
+        constructor($scope) {
+            $scope.selectClaim = function(value){
+                if(value === ''){
+                    $scope.foundClaim = false;
+                } else {
+                    var claimToPay = document.getElementsByClassName('claim-selected');
+                    $scope.foundClaim = true;
+                    claimToPay[0].textContent = value;
+                }
+            };
+        }
+    }
+
+    angular.module('healthxApp')
+        .controller('PayClaimCtrl', PayClaimCtrl);
+})();
