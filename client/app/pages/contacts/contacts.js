389ff9dd46c0c7ef895501d2af42edc229d74db8
Lawrence Moore <lawrence2978@hotmail.com>
2016-03-26 10:58:03 -0400

    Styles and wrote functionality for contacts

--- client/app/pages/contacts/contacts.js
+++ client/app/pages/contacts/contacts.js
@@ -1,11 +1,31 @@
 'use strict';
 
-angular.module('healthxApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('contacts', {
-        url: '/contacts',
-        templateUrl: 'app/pages/contacts/contacts.html',
-        controller: 'ContactsCtrl'
-      });
-  });
+(function() {
+    class ContactsCtrl {
+
+        constructor($scope) {
+            $scope.selectContact = function($event){
+                var listItem = angular.element($event.currentTarget),
+                    listItemValue = listItem[0].attributes[2].value,
+                    detailPages = angular.element('.contact-detail');
+                
+                for(var i = 0; i < detailPages.length; i++){
+                    if(listItemValue === detailPages[i].id){
+                        detailPages[i].classList.add('active')
+                    }
+                }
+            };
+
+            $scope.closeContact = function(){
+                var detailPages = angular.element('.contact-detail');
+
+                for(var i = 0; i < detailPages.length; i++){
+                    detailPages[i].classList.remove('active')
+                }
+            }
+        }
+    }
+
+    angular.module('healthxApp')
+        .controller('ContactsCtrl', ContactsCtrl);
+})();
